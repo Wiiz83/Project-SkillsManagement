581c46700e90d8f24401cd7f88caa5c840a3be97
Wiiz83 <uzanlah@gmail.com>
2017-02-26 12:02:37 +0100

    Test jtextfield

--- ProjetV1/src/navigation/Competences.java
+++ ProjetV1/src/navigation/Competences.java
@@ -1,5 +1,7 @@
 package navigation;
 
+import java.awt.BorderLayout;
+
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -10,12 +12,13 @@ public class Competences extends JPanel {
 	
 	public Competences () 
     {   
-	    setOpaque(false);
-		setLayout(null);
+	    setOpaque(true);
+		setLayout(new BorderLayout());
 		setBorder(BorderFactory.createEmptyBorder(9, 9, 9, 9));
 
 		this.nom = new JTextField();		
 		this.nom.setBounds(350, 50, 150, 40);
+
 		add(this.nom);
     }
 }
